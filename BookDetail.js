72c537f7399a783af4e1ab17d817ebb05548b105
Christian Borja <cpborja32@gmail.com>
2016-12-07 19:24:14 -0800

    starts BookDetail feature

---
+++ BookDetail.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image
+} from 'react-native';
+
+class BookDetail extends Component {
+  render() {
+    return (
+
+    );
+  }
+}
+
+let styles = StyleSheet.create({
+  container: {
+    marginTop: 75,
+    alignItems: 'center'
+  },
+  image: {
+    width: 107,
+    height: 165,
+    padding: 10
+  },
+  description: {
+    padding: 10,
+    fontSize: 15,
+    color: '#656565'
+  }
+});
