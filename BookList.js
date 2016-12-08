0aa5810c86ad538c5d8ab24b2a9c861ffe99bd02
Christian Borja <cpborja32@gmail.com>
2016-12-07 18:36:45 -0800

    adds ListView, fetches data from API

--- BookList.js
+++ BookList.js
@@ -3,31 +3,84 @@ import {
   StyleSheet,
   View,
   Text,
-  Image
+  Image,
+  ListView,
+  TouchableHighlight,
+  ActivityIndicator
 } from 'react-native';
 
 
-let FAKE_BOOK_DATA = [{
-  volumeInfo: {
-    title: 'The Catcher in the Rye',
-    authors: "J. D. Salinger",
-    imageLinks: {thumbnail: 'https://books.google.com/books/content?id=PCDengEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api'}
-  }
-}];
+let REQUEST_URL = 'https://www.googleapis.com/books/v1/volumes?q=subject:fiction';
 
 class BookList extends Component {
-  render() {
-    let book = FAKE_BOOK_DATA[0];
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: true,
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2
+      })
+    }
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(responseData.items),
+          isLoading: false
+        });
+      }).done();
+  }
+
+  renderBook(book) {
     return (
-      <View style={styles.container}>
-        <Image
-          source={{uri: book.volumeInfo.imageLinks.thumbnail}}
-          style={styles.thumbnail} />
-        <View style={styles.rightContainer}>
-          <Text style={styles.title}>{book.volumeInfo.title}</Text>
-          <Text style={styles.author}>{book.volumeInfo.authors}</Text>
+      <TouchableHighlight>
+        <View>
+          <View style={styles.container}>
+            <Image
+                source={{uri: `https:${book.volumeInfo.imageLinks.thumbnail.slice(5)}`}}
+                style={styles.thumbnail} />
+            <View style={styles.rightContainer}>
+                <Text style={styles.title}>{book.volumeInfo.title}</Text>
+                <Text style={styles.author}>{book.volumeInfo.authors}</Text>
+            </View>
+          </View>
+          <View style={styles.separator} />
         </View>
-      </View>
+      </TouchableHighlight>
+    );
+  }
+
+  // renderLoadingView() {
+  //   return (
+  //     <View style={styles.loading}>
+  //       <ActivityIndicator size='large' />
+  //       <Text>
+  //         Loading books...
+  //       </Text>
+  //     </View>
+  //   );
+  // }
+
+  render() {
+    // Causes issues with layout
+    // if(this.state.isLoading) {
+    //   return this.renderLoadingView();
+    // }
+
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderBook.bind(this)}
+        style={styles.listView}
+      />
     );
   }
 }
@@ -55,6 +108,18 @@ let styles = StyleSheet.create({
   },
   author: {
     color: '#656565'
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#dddddd'
+  },
+  listView: {
+    backgroundColor: '#F5FCFF'
+  },
+  loading: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 });
 
